185a8cd6146f38847c41e71f4878940113b7c3a3
SOE13 <soe48573@gmail.com>
2023-06-26 11:18:55 +0800

    datatable

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import React, { useState } from "react";
-import "./App.css";
-import Modal from "./Components/Modal";
+import React, { useEffect, useState } from "react";
+import DataTable from "react-data-table-component";
+
+const customStyles = {
+  headRow: {
+    style: {
+      backgroundColur: "blue",
+      // color: "white",
+    },
+    headCells: {
+      style: {
+        fontSize: "16px",
+        fontweight: "600",
+        textTransform: "uppercase",
+      },
+    },
+  },
+};
 
 function App() {
-  const [modalOpen, setModalOpen] = useState(false);
+  const column = [
+    { name: "ID", selector: (row) => row.id },
+    { name: "Name", selector: (row) => row.name },
+    { name: "Email", selector: (row) => row.email },
+    { name: "City", selector: (row) => row.address.city },
+  ];
 
+  useEffect(() => {
+    const fetData = async () => {
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then((response) => response.json())
+        .then((json) => setRecords(json))
+        .catch((err) => console.log(err));
+    };
+    fetData();
+  }, []);
+
+  const [records, setRecords] = useState([]);
   return (
-    <div className="App">
-      <h1>Hey, click on the button to open the modal.</h1>
-      <button
-        className="openModalBtn"
-        onClick={() => {
-          setModalOpen(true);
-        }}
-      >
-        Open
-      </button>
-
-      {modalOpen && <Modal setOpenModal={setModalOpen} />}
+    <div className={{}}>
+      <DataTable
+        columns={column}
+        data={records}
+        customStyles={customStyles}
+        pagination></DataTable>
     </div>
   );
 }
